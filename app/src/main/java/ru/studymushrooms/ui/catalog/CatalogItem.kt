5f51317ddf741c4653cd608a155da34edfcd9b11
stop-upiter <56906962+stop-upiter@users.noreply.github.com>
2022-05-15 18:43:37 +0300

    Update CatalogItem.kt

--- app/src/main/java/ru/studymushrooms/ui/catalog/CatalogItem.kt
+++ app/src/main/java/ru/studymushrooms/ui/catalog/CatalogItem.kt
@@ -48,15 +48,19 @@ class CatalogItem(val mushroom: MushroomModel) : Item() {
         }
 
         val image = viewHolder.itemView.findViewById<ImageView>(R.id.card_image)
-        Picasso.get().load(mushroom.pictureLink).into(image, Callback() {
-            override public void onError(Exception e) {
-                Toast.makeText(
-                    context,
-                    "Ошибка при загрузке картинки",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        })
+        val picasso = Picasso.Builder(context).listener(
+                    object : Picasso.Listener{
+                        override fun onImageLoadFailed(picasso: Picasso?, uri: Uri?, exception: Exception?) {
+                             Toast.makeText(
+                                context,
+                                "Ошибка при загрузке картинки",
+                                Toast.LENGTH_LONG
+                            `).show()
+                        }
+
+                    }
+            ).build()
+        picasso.load(mushroom.pictureLink).fit().into(image)
 
         val card = viewHolder.itemView.findViewById<MaterialCardView>(R.id.card)
         card.setOnClickListener {
